b04f1a284eb26ceb01c01fd41a984225fb6b6957
AL3X-dot <amitdave1998@gmail.com>
2021-02-01 10:37:26 +0530

    Add project files.

---
+++ Crud.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkTest
+{
+    internal class Crud
+    {
+        private static void Main(string[] args)
+        {
+            var emp = new Employee()
+            {
+                empName = "Jognesh",
+                empNo = 2000,
+                salary = 60000,
+                dptCode = 102,
+                mgrEmpNo = 1001,
+            };
+            var cemp = new CrudEmployee();
+            cemp.addEmployee(emp);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkTest
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //var dbContext = new EmployeeDatabaseEntities();
+            //var employee = new Employee()
+            //{
+            //    empId = 6,
+            //    empname = "Harshil shah",
+            //    salary = 45000
+            //};
+            ////dbContext.Employees.Add(employee);
+            ////dbContext.SaveChanges();
+            //var list = dbContext.Employees.ToList<Employee>();
+            //foreach (var emp in list)
+            //{
+            //    Console.WriteLine(emp.empId + "\t" + emp.empname + "\t" + emp.salary);
+            //}
+        }
+    }
+}
